7e09a362c89f0684641a37e5518301fd7639e11b
itsMontoya <josh@montoya.io>
2018-03-02 10:03:43 -0800

    Fix profiling

--- testing/profiling/main.go
+++ testing/profiling/main.go
@@ -7,8 +7,8 @@ import (
 	"strconv"
 	"time"
 
+	"github.com/itsmontoya/rbt"
 	"github.com/missionMeteora/journaler"
-
 	"github.com/pkg/profile"
 )
 
@@ -24,7 +24,7 @@ func main() {
 
 	for i := 0; i < 1000; i++ {
 		for _, kv := range strs {
-			tr.Put(kv.key, kv.val)
+			tr.Put(kv.val, kv.val)
 		}
 	}
 }
